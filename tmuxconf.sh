900dfdbaa7258c0124bcfe4640a8df1b75be7db2
nick-ng <nick-ng@users.noreply.github.com>
2025-08-20 23:05:49 +1200

    feat: update aurabot filter to level with econc

--- tmuxconf.sh
+++ tmuxconf.sh
@@ -2,8 +2,5 @@ MY_SESSION=$(tmux list-sessions | grep "poefilters")
 if [[ ! $MY_SESSION ]]; then
 		# create a new session and `-d`etach
 		tmux new-session -d -s poefilters
-		tmux send "nvim ." Enter
-		tmux new-window
-		# tmux split-window -h
 		tmux attach-session -d -t poefilters
 fi
