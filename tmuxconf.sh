e820fff60b58f0f4ed5877341557b544f0a99b91
nick-ng <nick-ng@users.noreply.github.com>
2025-08-23 09:45:03 +1200

    fix: attach to poe filters tmux session if you find it

--- tmuxconf.sh
+++ tmuxconf.sh
@@ -2,5 +2,5 @@ MY_SESSION=$(tmux list-sessions | grep "poefilters")
 if [[ ! $MY_SESSION ]]; then
 		# create a new session and `-d`etach
 		tmux new-session -d -s poefilters
-		tmux attach-session -d -t poefilters
 fi
+tmux attach-session -d -t poefilters
