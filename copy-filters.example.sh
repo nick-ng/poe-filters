b4870888b20028f543912c287f1cbc0e4b98e299
pux <nick-ng@users.noreply.github.com>
2026-08-08 22:14:32 +1200

    feat: remove verbose flag from filter copy script. add portal and wisdom scrolls to some juice filter

--- copy-filters.example.sh
+++ copy-filters.example.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # copy filters to secondary Path of Exile directories
-cp -v ~/.steam/steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/*.filter /path/to/secondary-poe-dir/
+cp -u ~/.steam/steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/*.filter /path/to/secondary-poe-dir/
 
 # copy tts to secondary Path of Exile directories
-cp -v -u ~/.steam/steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/tts/* /path/to/secondary-poe-dir/tts/
+cp -u ~/.steam/steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/tts/* /path/to/secondary-poe-dir/tts/
 
 # copy sounds to secondary Path of Exile directories
-cp -v -u ~/.steam/steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/sounds/* /path/to/secondary-poe-dir/sounds/
+cp -u ~/.steam/steam/steamapps/compatdata/238960/pfx/drive_c/users/steamuser/My\ Documents/My\ Games/Path\ of\ Exile/sounds/* /path/to/secondary-poe-dir/sounds/
