3a7df937c496fa17e19d9c3c1c49c1feebd5972e
pux <nick-ng@users.noreply.github.com>
2026-09-12 00:15:30 +1200

    feat: add heist filter back

--- tmuxconf.sh
+++ tmuxconf.sh
@@ -4,10 +4,7 @@ if [[ ! $MY_SESSION ]]; then
 		tmux new-session -d -s poefilters
 		tmux split-window -h
 		tmux send "go run . --watch" Enter
-		tmux split-window -v
 		tmux send "tmux resizep -x 25%" Enter
-		tmux send "cd ../poe-economy" Enter
-		tmux send "./update.sh" Enter
 		tmux select-pane -t 0
 fi
 tmux attach-session -d -t poefilters
